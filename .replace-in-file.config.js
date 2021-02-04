d1a25f52bc8d336b2e0fd874f4d03067ddc1e827
Ottó Radics <otto@webmenedzser.hu>
2021-02-04 11:35:11 +0100

    Removed version key from composer.json

--- .replace-in-file.config.js
+++
@@ -1,9 +0,0 @@
-const pkg = require('./package.json');
-
-module.exports = {
-    files: "composer.json",
-    from: '/"version": "(.*?)"/g',
-    to: '"version": "' + pkg.version + '"',
-    isRegex: true,
-    dry: false
-}
